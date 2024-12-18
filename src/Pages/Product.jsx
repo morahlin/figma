6afa8a9d35eb95922871a6e1ac42bbd94a49b68d
obinna <morahlinmorahmorahlin@gmail.com>
2024-12-18 10:08:42 +0100

    figma

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -10,13 +10,19 @@ import car8 from '../assets/car8.jpg';
 import car9 from '../assets/car9.jpg';
 import car11 from '../assets/car11.jpg';
 import car12 from '../assets/car12.jpg';
-import car13 from '../assets/car13.jpg';
+import car13 from '../assets/car1.jpg';
+import { motion, useAnimation } from 'framer-motion';
+
+
 
 
 const Product = () => {
+  const animation = useAnimation();
   return (
     <div className='pt-[10vh]'>
       <div className='flex gap-20 items-center justify-center flex-wrap p-10 md:p-1'>
+      <motion.div initial={{ x: 500, opacity: 0 }} animate={{ x: 0, opacity: 5, transition: { duration: 3} }}>
+
         
       <div>
         <h1 className='text-[25px]'>Toyota Camry
@@ -25,14 +31,18 @@ const Product = () => {
         <p>  &#8358;100,000,000</p>
         <button className='w-[139px] h-[50px] rounded-[3px] mt-2 bg bg-red-600 text bg-center justify-center items-center'>buy now</button>
       </div>
+      </motion.div>
 
-      <div>
-        <h1 className='text-[25px]'>Honda CR-V
-        </h1>
-        <img src={car2} alt='/' width={300} />
-        <p>  &#8358;200,000,000</p>
-        <button className='w-[139px] h-[50px] rounded-[3px] mt-2 bg bg-red-600 text bg-center justify-center items-center'>buy now</button>
-      </div>
+      <motion.div initial={{ x: 500, opacity: 0 }} animate={{ x: 0, opacity: 7, transition: { duration: 4 } }}>
+
+  <div>
+    <h1 className='text-[25px]'>Volkswagen Golf </h1>
+    <img src={car3} alt='/' width={300} />
+    <p> &#8358;100,000,000</p>
+    <button className='w-[139px] h-[50px] rounded-[3px] mt-2 bg bg-red-600 text bg-center justify-center items-center'>buy now</button>
+  </div>
+</motion.div>
+<motion.div initial={{ x: 500, opacity: 0 }} animate={{ x: 0, opacity: 1, transition: { duration: 4 } }}>
 
       <div>
         <h1 className='text-[25px]'>Ford F-150
@@ -41,6 +51,10 @@ const Product = () => {
         <p>  &#8358;500,000,000</p>
         <button className='w-[139px] h-[50px] rounded-[3px] mt-2 bg bg-red-600 text bg-center justify-center items-center'>buy now</button>
       </div>
+      </motion.div>
+      <motion.div initial={{ x: 500, opacity: 0 }} animate={{ x: 0, opacity: 1, transition: { duration: 3 } }}>
+
+
       <div>
         <h1 className='text-[25px]'>Volkswagen Golf
         </h1>
@@ -48,6 +62,9 @@ const Product = () => {
         <p>  &#8358;100,000,000</p>
         <button className='w-[139px] h-[50px] rounded-[3px] mt-2 bg bg-red-600 text bg-center justify-center items-center'>buy now</button>
       </div>
+      </motion.div>
+      <motion.div initial={{ x: 500, opacity: 0 }} animate={{ x: 0, opacity: 1, transition: { duration: 4} }}>
+
       <div>
         <h1 className='text-[25px]'>BMW 4 Series
         </h1>
@@ -55,6 +72,9 @@ const Product = () => {
         <p>  &#8358;100,000,000</p>
         <button className='w-[139px] h-[50px] rounded-[3px] mt-2 bg bg-red-600 text bg-center justify-center items-center'>buy now</button>
       </div>
+      </motion.div>
+      <motion.div initial={{ x: 500, opacity: 0 }} animate={{ x: 0, opacity: 1, transition: { duration: 4 } }}>
+
       <div>
         <h1 className='text-[25px]'>Mazda MX-5 Miata
         </h1>
@@ -62,46 +82,73 @@ const Product = () => {
         <p> &#8358; 100,000,000</p>
         <button className='w-[139px] h-[50px] rounded-[3px] mt-2 bg bg-red-600 text bg-center justify-center items-center'>buy now</button>
       </div>
+      </motion.div>
+
+
+      <motion.div initial={{ x: 500, opacity: 0 }} animate={{ x: 0, opacity: 1, transition: { duration: 3 } }}>
+
       <div>
-        <h1 className='text-[25px]'>white car</h1>
+        <h1 className='text-[25px]'>GMC Sierra
+        </h1>
         <img src={car7} alt='/' width={300} />
         <p> &#8358;900,000,000</p>
         <button className='w-[139px] h-[50px] rounded-[3px] mt-2 bg bg-red-600 text bg-center justify-center items-center'>buy now</button>
       </div>
+      </motion.div>
+      
+      <motion.div initial={{ x: 500, opacity: 0 }} animate={{ x: 0, opacity: 1, transition: { duration: 4 } }}>
+
       <div>
-        <h1 className='text-[25px]'>white car</h1>
+        <h1 className='text-[25px]'>Jaguar E-Type
+        </h1>
         <img src={car8} alt='/' width={300} />
         <p> &#8358;700,000,000</p>
         <button className='w-[139px] h-[50px] rounded-[3px] mt-2 bg bg-red-600 text bg-center justify-center items-center'>buy now</button>
       </div>
+
+      </motion.div>
+      <motion.div initial={{ x: 500, opacity: 0 }} animate={{ x: 0, opacity: 1, transition: { duration: 4 } }}>
+
       <div>
-        <h1 className='text-[25px]'>white car</h1>
+        <h1 className='text-[25px]'>Porsche 356
+        </h1>
         <img src={car9} alt='/' width={300} />
         <p> &#8358;600,000,000</p>
         <button className='w-[139px] h-[50px] rounded-[3px] mt-2 bg bg- bg-red-600 text bg-center justify-center items-center'>buy now</button>
       </div>
-
+      </motion.div>
+      <motion.div initial={{ x: 500, opacity: 0 }} animate={{ x: 0, opacity: 1, transition: { duration: 3 } }}>
       <div>
-        <h1 className='text-[25px]'>white car</h1>
+        <h1 className='text-[25px]'>Jaguar E-Type
+        </h1>
         <img src={car13} alt='/' width={300} />
         <p>  &#8358;600,000,000</p>
         <button className='w-[139px] h-[50px] rounded-[3px] mt-2 bg bg- bg-red-600 text bg-center justify-center items-center'>buy now</button>
       </div>
+      </motion.div>
 
+      <motion.div initial={{ x: 500, opacity: 0 }} animate={{ x: 0, opacity: 1, transition: { duration: 4 } }}>
       <div>
-        <h1 className='text-[25px]'>white car</h1>
-        <img src={car11} alt='/' width={300} />
-        <p> &#8358;600,000,000</p>
-        <button className='w-[139px] h-[50px] rounded-[3px] mt-2 bg bg- bg-red-600 text bg-center justify-center items-center'>buy now</button>
+        <h1 className="text-[25px]">Jaguar E-Type
+        </h1>
+        <img src={car11} alt="/" width={300} />
+        <p>&#8358;600,000,000</p>
+        <button className="w-[139px] h-[50px] rounded-[3px] mt-2 bg-red-600 text-center justify-center items-center">
+          Buy Now
+        </button>
       </div>
+      </motion.div>
 
+      <motion.div initial={{ x: 500, opacity: 0 }} animate={{ x: 0, opacity: 1, transition: { duration: 4 } }}>
       <div>
-        <h1 className='text-[25px]'>white car</h1>
+        <h1 className='text-[25px]'>Ram 1500
+        </h1>
         <img src={car12} alt='/' width={300} />
         <p> &#8358;600,000,000</p>
         <button className='w-[139px] h-[50px] rounded-[3px] mt-2 bg bg- bg-red-600 text bg-center justify-center items-center'>buy now</button>
       </div>
-
+      </motion.div>
+  
       </div> 
   
 
